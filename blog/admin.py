f46c9677366d792af764dacdf6fc0109d84d6e62
sagargajare <sggajare360@gmail.com>
2021-01-10 12:55:29 +0530

    test1

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post ,Updates
+from .models import Post ,Updates , upcomingEvents ,pastEvents
 
 admin.site.register(Post)
 admin.site.register(Updates)
+admin.site.register(upcomingEvents)
+admin.site.register(pastEvents)
