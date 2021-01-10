0e94be6213f01a0f98276e586092cc69f12a1ec5
sagargajare <sggajare360@gmail.com>
2021-01-10 13:33:32 +0530

    a

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -194,13 +200,7 @@ AUTHENTICATION_BACKENDS = [
 
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 ACCOUNT_LOGOUT_ON_GET =True
 django_heroku.settings(locals())
 # acm
